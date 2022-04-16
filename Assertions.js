6a1fb304dc7a5bb3bb4634bc888b91d7b3f8056b
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-04-16 13:00:12 +0530

    1st commit

---
+++ Assertions.js
@@ -0,0 +1,19 @@
+describe('Assertions in Web DriverIO', () => {
+
+    it('First Drop down', async () => {
+        await browser.url('https://rahulshettyacademy.com/loginpagePractise/');
+        console.log(await browser.getTitle());
+        await $("#username").setValue("rahulshettyacademy");
+        await $('#password').setValue("learning");
+        // var radioButton = await $$('.customradio');
+        // await radioButton[1].$("input[id='usertype']").click();
+        // const popup = $(".modal-content")
+        // await popup.waitForDisplayed();
+        // (await $("button[id='cancelBtn']")).click();
+        // const Dropdown = await $("select.form-control");
+        // await Dropdown.selectByAttribute('value', 'consult');
+        // await browser.pause('4000');
+
+
+    });
+})
