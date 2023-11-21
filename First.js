b7f86c579edbc25688dd9ef629df0b575801378f
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-11-21 08:56:25 +0530

    Update First.js

--- First.js
+++ First.js
@@ -4,6 +4,7 @@
 //https://www.selenium.dev/documentation/webdriver/elements/
 
 var AllureReporter = require('jasmine-allure-reporter');
+
 jasmine.getEnv().addReporter(new AllureReporter({
     resultsDir: 'allure-results'
 }));
