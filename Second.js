34cc9130a1c3dcc74223c2dbf7a004f2fc1ec679
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-11-23 09:44:32 +0530

    Update Second.js

--- Second.js
+++ Second.js
@@ -2,7 +2,6 @@
 //https://www.browserstack.com/docs/automate/selenium/getting-started/nodejs/run-tests-in-parallel
 
 //https://www.selenium.dev/documentation/webdriver/elements/
-
 var AllureReporter = require('jasmine-allure-reporter');
 
 jasmine.getEnv().addReporter(new AllureReporter({
