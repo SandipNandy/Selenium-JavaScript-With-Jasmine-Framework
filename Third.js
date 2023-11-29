d3693b1f6ffd606584ab616a158c4c3b6399eba7
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-11-29 10:09:58 +0530

    Update Third.js

--- Third.js
+++ Third.js
@@ -5,7 +5,6 @@
 //https://www.selenium.dev/documentation/webdriver/elements/
 
 var AllureReporter = require('jasmine-allure-reporter');
-
 jasmine.getEnv().addReporter(new AllureReporter({
     
     resultsDir: 'allure-results'
