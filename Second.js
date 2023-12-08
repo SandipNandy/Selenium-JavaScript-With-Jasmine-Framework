338f81915f71149613e008ba866288243c9b6f97
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-08 09:46:15 +0530

    Update Second.js

--- Second.js
+++ Second.js
@@ -4,7 +4,6 @@
 //https://www.selenium.dev/documentation/webdriver/elements/
 var AllureReporter = require('jasmine-allure-reporter');
 jasmine.getEnv().addReporter(new AllureReporter({
-    
     resultsDir: 'allure-results'
 }));
 const { By, Key, Builder } = require("selenium-webdriver");
