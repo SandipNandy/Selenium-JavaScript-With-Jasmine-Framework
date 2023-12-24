840f71af1cfb70bd702961376877c5f2724e561b
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-24 07:59:56 +0530

    Update Second.js

--- Second.js
+++ Second.js
@@ -9,6 +9,7 @@ jasmine.getEnv().addReporter(new AllureReporter({
     resultsDir: 'allure-results'
 }));
 const { By, Key, Builder } = require("selenium-webdriver");
+
 const webdriver = require('selenium-webdriver');
 require("chromedriver");
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 20 * 1000;
