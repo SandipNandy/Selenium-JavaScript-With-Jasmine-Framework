f39a5a78155e6fe2b96a883e27f855cf060224d1
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-24 07:55:08 +0530

    Update IframesConceptsUpdate.js

--- IframesConceptsUpdate.js
+++ IframesConceptsUpdate.js
@@ -10,7 +10,6 @@ require("chromedriver");
 //require("geckodriver");
 //require("iedriver");
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 80 * 1000;
-
 jasmine.getEnv().defaultTimeoutInterval = 60000;
 
 //const driver = new webdriver.Builder().usingServer('http://192.168.0.100:4444/wd/hub').forBrowser("chrome").build();
