b35ebcd23b84d24b198756a36bd307be1dd4101a
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-24 07:54:59 +0530

    Update IframesConceptsUpdate.js

--- IframesConceptsUpdate.js
+++ IframesConceptsUpdate.js
@@ -5,7 +5,6 @@ jasmine.getEnv().addReporter(new AllureReporter({
 }));
 const { By, Key, Builder } = require("selenium-webdriver");
 const webdriver = require('selenium-webdriver');
-
 require("chromedriver");
 
 //require("geckodriver");
