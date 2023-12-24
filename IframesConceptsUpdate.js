554294c423a0ccce7da90f6628d2ec62216e9d05
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-24 07:57:02 +0530

    Update IframesConceptsUpdate.js

--- IframesConceptsUpdate.js
+++ IframesConceptsUpdate.js
@@ -30,7 +30,6 @@ describe('First Test', function () {
         //await driver.switchTo().frame('frame-id');
         //switch to frame using name
         //await driver.switchTo().frame('frame-name');
-
         //switch to frame using web element
         //await driver.switchTo().frame(driver.findElement(By.webLocator('frame-web-locator-property')));
         //await driver.sleep(3000);
