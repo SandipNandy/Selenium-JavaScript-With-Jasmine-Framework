7df727b1d97294836dcbf11e73b9957eb60ff1a5
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-30 09:21:32 +0530

    Update Assertions.js

--- Assertions.js
+++ Assertions.js
@@ -1,7 +1,6 @@
 describe('Assertions in Web DriverIO', () => {
 
     it('First Drop down', async () => {
-        
         await browser.url('https://rahulshettyacademy.com/loginpagePractise/');
         
         console.log(await browser.getTitle());
