28d69ec5a10e88e7f45c6dd61d5e349b277c49d9
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-01-13 10:34:26 +0530

    Update IframesConceptsUpdate.js

--- IframesConceptsUpdate.js
+++ IframesConceptsUpdate.js
@@ -1,4 +1,5 @@
 var AllureReporter = require('jasmine-allure-reporter');
+
 const { until } = require('selenium-webdriver');
 jasmine.getEnv().addReporter(new AllureReporter({
     resultsDir: 'allure-results'
