28bcd3ac0612160b7c3d1fe5efbeb6c84f6bf48b
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-01-20 11:35:23 +0530

    Update WindowHandles.js

--- WindowHandles.js
+++ WindowHandles.js
@@ -1,7 +1,6 @@
 var AllureReporter = require('jasmine-allure-reporter');
 jasmine.getEnv().addReporter(new AllureReporter({
     resultsDir: 'allure-results'
-    
 }));
 
 const { By, Key, Builder } = require("selenium-webdriver");
