589dd7e868464a3d76712ee69ca4ddae68b02211
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-02-18 07:30:48 +0530

    Update Assertions.js

--- Assertions.js
+++ Assertions.js
@@ -7,7 +7,6 @@ describe('Assertions in Web DriverIO', () => {
         
         // var radioButton = await $$('.customradio');
         // await radioButton[1].$("input[id='usertype']").click();
-        
         // const popup = $(".modal-content")
         // await popup.waitForDisplayed();
         // (await $("button[id='cancelBtn']")).click();
