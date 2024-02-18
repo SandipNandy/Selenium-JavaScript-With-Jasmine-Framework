1e478e113e62e31a35d061d985072bd4c6fd3eae
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-02-18 07:33:13 +0530

    Update First.js

--- First.js
+++ First.js
@@ -16,7 +16,6 @@ jasmine.getEnv().defaultTimeoutInterval = 60000;
 
 const driver = new webdriver.Builder().usingServer('http://192.168.0.101:4444/wd/hub').forBrowser("chrome").build();
 describe('First Test', function () {
-    
     it('My first it block', function () {
         
         console.log('<----- Starting to execute test case ----->');
