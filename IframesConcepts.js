ca055da7d4c02f06d40da52aa5f67be6512606c2
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-06-02 15:53:14 +0530

    Update IframesConcepts.js

--- IframesConcepts.js
+++ IframesConcepts.js
@@ -27,7 +27,6 @@ var driver = new Builder().forBrowser("chrome").build();
 
 describe('First Test', function () {
     it('Launch URL', async function () {
-        
         await driver.get("https://courses.letskodeit.com/practice");
         
         await driver.manage().window().maximize();
