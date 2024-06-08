a7df2b14b33ba88a4d1cc9981bb909293dd6b147
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-06-08 23:17:14 +0530

    Update IframesConcepts.js

--- IframesConcepts.js
+++ IframesConcepts.js
@@ -19,7 +19,6 @@ jasmine.getEnv().defaultTimeoutInterval = 60000;
 
 
 var driver = new Builder().forBrowser("chrome").build();
-
 describe('First Test', function () {
     it('Launch URL', async function () {
         await driver.get("https://courses.letskodeit.com/practice");
