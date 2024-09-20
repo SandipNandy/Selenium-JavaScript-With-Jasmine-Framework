e8093aea9892845d4369d83172e1d15a1ce4b51b
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-09-20 10:15:19 +0530

    Update Third.js

--- Third.js
+++ Third.js
@@ -9,7 +9,6 @@ jasmine.getEnv().addReporter(new AllureReporter({
 const { By, Key, Builder } = require("selenium-webdriver");
 const webdriver = require('selenium-webdriver');
 require("chromedriver");
-
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 20 * 1000;
 
 jasmine.getEnv().defaultTimeoutInterval = 60000;
